c5674089c767068b7d25e102cb265368a91555a5
daniel <daniel.d4@icloud.com>
2019-11-20 16:04:16 +0100

    Initial Commit

---
+++ Todo WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Todo WatchKit Extension
+//
+//  Created by daniel d4 on 20/11/2019.
+//  Copyright © 2019 Remi Herbiet. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Todo WatchKit Extension/NotificationController.swift
@@ -0,0 +1,34 @@
+//
+//  NotificationController.swift
+//  Todo WatchKit Extension
+//
+//  Created by daniel d4 on 20/11/2019.
+//  Copyright © 2019 Remi Herbiet. All rights reserved.
+//
+
+import WatchKit
+import SwiftUI
+import UserNotifications
+
+class NotificationController: WKUserNotificationHostingController<NotificationView> {
+
+    override var body: NotificationView {
+        return NotificationView()
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
